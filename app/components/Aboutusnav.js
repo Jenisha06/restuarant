9f74ac420ae40e1a4d15ee46082c6c6cae6859d3
Jenisha Dsouza <jen06dsouza@gmail.com>
2025-11-11 17:03:05 +0530

    Added Aboutusnav component

---
+++ app/components/Aboutusnav.js
@@ -0,0 +1,35 @@
+
+export default function Aboutusnav() {
+  return (
+         <div className="flex  font-medium  pt-4  pb-3 bg-[hsl(32,28%,40%)] text-white">
+  <div className="flex gap-3 justify-center items-center ml-25">
+
+      <img src="call-icon.webp" className="h-5 "></img>
+    <a href="" target="_blank"  >
+ Phone: +4733378901
+</a>
+<img src="	https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2F7.png&w=64&q=75
+" className="h-5 ml-5"></img>
+<a href="" target="_blank" >
+  Email:food@restan.com
+</a>
+</div>
+<div className="flex gap-3 items-center ml-145 border-r border-white pr-8">
+  <img src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2Fflag.png&w=64&q=75" className="h-6"></img>
+  <p  className="text-white">English 
+    <i className="fa fa-chevron-down text-[15px] px-1.5 text-white  "></i>
+    
+  </p>
+ 
+
+</div>
+<div className="flex gap-4 ml-6 text-white items-center text-lg">
+  <i class="fab fa-facebook-f" ></i>
+  <i class="fab fa-twitter"></i>
+  <i class="fab fa-youtube"></i>
+  <i class="fab fa-linkedin-in"></i>
+</div>
+
+    </div>
+  )
+}
