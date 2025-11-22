ab2513854d1f4e19e4f877301a582bce6d2e01f0
Jenisha Dsouza <jen06dsouza@gmail.com>
2025-11-22 11:36:51 +0530

    Updateblog

--- app/BlogStandard/page.js
+++ app/BlogStandard/page.js
@@ -1,6 +1,7 @@
 import Aboutusnav from '../components/Aboutusnav';
 import CommonNavbar from '../components/CommonNavbar';
 import Footer from '../components/Footer';
+import MainImage from '../components/MainImage';
 
 
 export default function BlogPage() {
@@ -12,7 +13,7 @@ export default function BlogPage() {
    
  <Aboutusnav/>
  <CommonNavbar/>
- <MainImage/>
+ <MainImage MainHead={"Blog Page"} SubHead={"Blog"}/>
  
  
  <Footer/>
@@ -24,31 +25,4 @@ export default function BlogPage() {
 
 
 
-function MainImage() {
-  return (
- <div className="relative w-full h-96">
- 
-  <img
-    src="https://restan-nextjs.vercel.app/assets/img/shape/5.jpg"
-    alt="About Us Background"
-    className="absolute inset-0 w-full h-full object-cover"
-  />
-
-
-
-  
-  <div className="relative z-20 flex flex-col justify-center items-center h-full text-white text-center">
-    <h1 className="text-5xl font-bold mb-4 font-marcellus">Blog Standard</h1>
-    <div className="flex items-center font-medium text-sm">
-      <i className="fas fa-home text-white text-base "></i>
-      <p className="ml-2">Home</p>
-      <i className="fa fa-chevron-right text-white text-xs px-1.5 "></i>
-      <p>about-us</p>
-    </div>
-  </div>
-</div>
-
-
-  )
-}
 
