b0fef3df8c900df32eb724c3d486ce600491ff35
Jenisha Dsouza <jen06dsouza@gmail.com>
2025-11-22 06:16:30 +0530

    register page reponsive

--- app/Register/page.js
+++ app/Register/page.js
@@ -51,16 +51,16 @@ function MainImage() {
 }
 function RegisterForm() {
   return (
-    <div className="flex w-[85%] mx-auto min-h-screen items-center justify-center gap-9">
+    <div className="flex flex-col sm:flex-row w-[85%] mx-auto  items-center justify-center sm:gap-9">
      
       <img
         src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fthumb%2F14.jpg&w=1920&q=75"
         alt="Dining"
-        className="w-[50%] h-[650px] object-cover "
+        className="h-[500px] sm:w-[50%] sm:h-[650px] object-cover "
       />
 
     
-      <div className="w-[45%] bg-white p-8 ">
+      <div className="w-[90%] sm:w-[45%] bg-white p-8 ">
         <h2 className="text-3xl font-marcellus font-bold text-[#826a45] mb-6">CREATE AN ACCOUNT</h2>
         <p className="text-[15px] text-gray-600 mb-6">
           Enter your details to create a new account
@@ -70,35 +70,35 @@ function RegisterForm() {
           <input
             type="email"
             placeholder="Email"
-            className="px-4 py-3  bg-gray-200 rounded-md text-black"
+            className="px-4 py-5  bg-gray-200 rounded-md text-black"
           />
           <input
             type="password"
             placeholder="Password"
-            className="px-4 py-3  bg-gray-200 rounded-md text-black"
+            className="px-4 py-5  bg-gray-200 rounded-md text-black"
           />
           <input
             type="password"
             placeholder="Confirm Password"
-            className="px-4 py-3  bg-gray-200 rounded-md text-black"
+            className="px-4 py-5  bg-gray-200 rounded-md text-black"
           />
 
-          <button className="bg-[#826a45]  text-white font-semibold py-3 text-[17px] rounded-md ">
+          <button className="bg-[#826a45]  text-white font-semibold py-4 text-[17px] rounded-md ">
            REGISTER
           </button>
 <h2 className='items-center text-2xl font-marcellus flex justify-center font-semibold pt-4'>Or Register With</h2>
           <div className="flex items-center justify-between mt-4">
           
-            <button className=" flex   items-center p-10 w-[240px] border border-gray-300  px-4 py-2 rounded-md gap-3  font-semibold">
+            <button className=" flex   items-center p-10 w-[240px] border border-gray-300  px-4 py-3 rounded-md gap-3  font-semibold">
              <div className='h-8 w-8 bg-red-500 rounded-full text-white items-center flex justify-center ml-7'>  <i class="fab fa-google"> </i></div> Google</button>
 
- <button className=" flex   items-center p-10 w-[240px] border border-gray-300  px-4 py-2 rounded-md gap-3  font-semibold">
+ <button className=" flex   items-center p-10 w-[240px] border border-gray-300  px-4 py-3 rounded-md gap-3  font-semibold">
              <div className='h-8 w-8 bg-[#3b5998] rounded-full text-white items-center flex justify-center ml-7'>  <i class="fab fa-facebook-f"></i></div> Facebook</button>
           </div>
 
-          <p className="text-sm text-center mt-6">
+          <p className="text-[15px] text-gray-600  text-center mt-6">
             Already have an account?{" "}
-            <a href="#" className="text-red-500 font-semibold">
+            <a href="#" className="text-red-500 font-bold ">
               Login Now
             </a>
           </p>
