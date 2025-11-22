8c69b7230d7c5e841edb9986453f75dbc02cb3a4
Jenisha Dsouza <jen06dsouza@gmail.com>
2025-11-22 11:46:38 +0530

    Update eservation

--- app/Reservation/page.js
+++ app/Reservation/page.js
@@ -1,6 +1,7 @@
 import Aboutusnav from '../components/Aboutusnav';
 import CommonNavbar from '../components/CommonNavbar';
 import Footer from '../components/Footer';
+import MainImage from '../components/MainImage';
 
 
 export default function ChefPage() {
@@ -12,7 +13,7 @@ export default function ChefPage() {
    
  <Aboutusnav/>
  <CommonNavbar/>
- <MainImage/>
+  <MainImage MainHead={"Chef Page"} SubHead={"Chef Page"}/>
  <Booking/>
  
  <Footer/>
@@ -22,33 +23,8 @@ export default function ChefPage() {
   </div>)
 }
 
-function MainImage() {
-  return (
- <div className="relative w-full h-96">
- 
-  <img
-    src="https://restan-nextjs.vercel.app/assets/img/shape/5.jpg"
-    alt="About Us Background"
-    className="absolute inset-0 w-full h-full object-cover"
-  />
 
 
-
-  
-  <div className="relative z-20 flex flex-col justify-center items-center h-full text-white text-center">
-    <h1 className="text-5xl font-bold mb-4 font-marcellus">Online Reservation</h1>
-    <div className="flex items-center font-medium text-sm">
-      <i className="fas fa-home text-white text-base "></i>
-      <p className="ml-2">Home</p>
-      <i className="fa fa-chevron-right text-white text-xs px-1.5 "></i>
-      <p>Reservation</p>
-    </div>
-  </div>
-</div>
-
-
-  )
-}
 
 function Booking(){
     return(
@@ -121,3 +97,4 @@ function Booking(){
         </div>
     )
 }
+
