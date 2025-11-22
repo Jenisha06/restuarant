9d3e67502ee7b784c53fee963993cc6155b2d0e6
Jenisha Dsouza <jen06dsouza@gmail.com>
2025-11-22 11:40:16 +0530

    Update chef details

--- app/ChefDetails/page.js
+++ app/ChefDetails/page.js
@@ -1,6 +1,7 @@
 import Aboutusnav from '../components/Aboutusnav';
 import CommonNavbar from '../components/CommonNavbar';
 import Footer from '../components/Footer';
+import MainImage from '../components/MainImage';
 
 
 export default function ChefPage() {
@@ -12,7 +13,7 @@ export default function ChefPage() {
    
  <Aboutusnav/>
  <CommonNavbar/>
- <MainImage/>
+  <MainImage MainHead={"Chef page"} SubHead={"chef page"}/>
  <ChefCard/>
  <Details/>
  <Footer/>
@@ -24,33 +25,7 @@ export default function ChefPage() {
 
 
 
-function MainImage() {
-  return (
- <div className="relative w-full h-96">
- 
-  <img
-    src="https://restan-nextjs.vercel.app/assets/img/shape/5.jpg"
-    alt="About Us Background"
-    className="absolute inset-0 w-full h-full object-cover"
-  />
 
-
-
-  
-  <div className="relative z-20 flex flex-col justify-center items-center h-full text-white text-center">
-    <h1 className="text-5xl font-bold mb-4 font-marcellus">Chef Details</h1>
-    <div className="flex items-center font-medium text-sm">
-      <i className="fas fa-home text-white text-base "></i>
-      <p className="ml-2">Home</p>
-      <i className="fa fa-chevron-right text-white text-xs px-1.5 "></i>
-      <p>about-us</p>
-    </div>
-  </div>
-</div>
-
-
-  )
-}
 
 function ChefCard() {
   return (
@@ -186,4 +161,5 @@ function Details(){
   )
 
 }
+
 
