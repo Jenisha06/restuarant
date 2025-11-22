bca99a2a7d5f533b15bf49d7c8d63b4c00028a1f
Jenisha Dsouza <jen06dsouza@gmail.com>
2025-11-22 06:16:02 +0530

    reservation responsiveness

--- app/Reservation/page.js
+++ app/Reservation/page.js
@@ -41,7 +41,7 @@ function MainImage() {
       <i className="fas fa-home text-white text-base "></i>
       <p className="ml-2">Home</p>
       <i className="fa fa-chevron-right text-white text-xs px-1.5 "></i>
-      <p>about-us</p>
+      <p>Reservation</p>
     </div>
   </div>
 </div>
@@ -52,37 +52,37 @@ function MainImage() {
 
 function Booking(){
     return(
-        <div className='mb-5 mt-20 '> 
-            <h2 className='text-[#04000b] opacity-10 font-marcellus text-9xl font-semibold ml-15'>RESTAN</h2>
-            <div className='flex flex-row'>
-<div className="relative w-[696px] h-[392px] pb-[56.25%] overflow-hidden ml-15 ">
+        <div className=' mt-20 sm:ml-35'> 
+            <h2 className='hidden sm:block sm:text-[#04000b] sm:opacity-10 font-marcellus sm:text-9xl sm:font-semibold sm:ml-15'>RESTAN</h2>
+            <div className='sm:flex sm:flex-row'>
+<div className="w-[500px]  relative sm:w-[780px] h-[500px] sm:pb-[56.25%]  sm:ml-15 ">
       <iframe
         src="https://www.youtube.com/embed/F3zw1Gvn4Mk?autoplay=1&loop=1&playlist=F3zw1Gvn4Mk&controls=0&preload=metadata&enablejsapi=1&showinfo=0&rel=0&iv_load_policy=3&modestbranding=1"
         title="Mr Fox Restaurant Promo Video"
         
       
-        className="absolute top-0 left-0 w-full h-[50%]"
+        className="absolute top-0 left-0 w-full h-[60%]"
        
       />
     </div>
 
-  <div className="bg-white -mt-15 -ml-25   text-black w-[550px] h-[650px] flex flex-col justify-center items-start p-3 shadow-md  z-10">
+  <div className="bg-white sm:-mt-15 sm:-ml-25   text-black sm:w-[550px] sm:h-[700px] flex flex-col justify-center items-start p-3 shadow-md  z-10">
         
 
 
        
-        <h2 className="font-bold self-start text-4xl ml-2 pb-3 pl-3 font-marcellus">Book a Table</h2>
+        <h2 className="font-bold self-start text-5xl mb-10 ml-2 pb-3 pl-3 font-marcellus">Book a Table</h2>
         <form className="flex-col flex items-start gap-5 text-left pl-5 w-full">
           <input
           type="tel"
           placeholder="Phone"
 
-          className="bg-white outline-gray-500 p-3 border border-gray-300 rounded-sm w-[90%] "
+          className="bg-white outline-gray-500 p-4 border border-gray-300 rounded-sm w-[90%] "
           />
 
          
 
-          <select className=" p-3 text-gray-950 border border-gray-300 rounded-sm w-[90%]">
+          <select className=" p-4 text-gray-950 border border-gray-300 rounded-sm w-[90%]">
 
           <option value="1">1 Person</option>
           <option value="2">2 Person</option>
@@ -94,10 +94,10 @@ function Booking(){
           <input
           type="date"
           placeholder="Date"
-          className="bg-white p-3 text-gray-500 outline-gray-500 border border-gray-300 rounded-sm w-[90%]"
+          className="bg-white p-4 text-gray-500 outline-gray-500 border border-gray-300 rounded-sm w-[90%]"
           />
 
-          <select className="border border-gray-300 rounded-sm w-[90%] p-3 text-gray-950">
+          <select className="border border-gray-300 rounded-sm w-[90%] p-4 text-gray-950">
 
           <option value="1">10:00 PM</option>
           <option value="2">11:00 PM</option>
@@ -108,7 +108,7 @@ function Booking(){
 
 
           <div className="flex justify-center items-center w-full">
-          <button className="bg-[#836849] hover:bg-black text-white px-4 py-4 rounded-sm w-[50%] font-medium mt-5 mb-8">
+          <button className="bg-[#836849] hover:bg-black text-white px-5 py-5 rounded-sm w-[50%] font-medium mt-5 mb-8">
 
             Book A Table
           </button>
