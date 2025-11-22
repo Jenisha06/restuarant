ea710d4f5c0d452e3dac643a0f6f7bf6520d7ca0
Jenisha Dsouza <jen06dsouza@gmail.com>
2025-11-22 06:21:14 +0530

    blopage repsponsivness

---
+++ app/BlogStandard/page.js
@@ -0,0 +1,54 @@
+import Aboutusnav from '../components/Aboutusnav';
+import CommonNavbar from '../components/CommonNavbar';
+import Footer from '../components/Footer';
+
+
+export default function BlogPage() {
+  return (<div>
+    <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+/>
+   
+ <Aboutusnav/>
+ <CommonNavbar/>
+ <MainImage/>
+ 
+ 
+ <Footer/>
+
+ 
+ 
+  </div>)
+}
+
+
+
+function MainImage() {
+  return (
+ <div className="relative w-full h-96">
+ 
+  <img
+    src="https://restan-nextjs.vercel.app/assets/img/shape/5.jpg"
+    alt="About Us Background"
+    className="absolute inset-0 w-full h-full object-cover"
+  />
+
+
+
+  
+  <div className="relative z-20 flex flex-col justify-center items-center h-full text-white text-center">
+    <h1 className="text-5xl font-bold mb-4 font-marcellus">Blog Standard</h1>
+    <div className="flex items-center font-medium text-sm">
+      <i className="fas fa-home text-white text-base "></i>
+      <p className="ml-2">Home</p>
+      <i className="fa fa-chevron-right text-white text-xs px-1.5 "></i>
+      <p>about-us</p>
+    </div>
+  </div>
+</div>
+
+
+  )
+}
+
